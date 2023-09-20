6df2b9d9f149aaaf3fe6d416ac1fcb19438e2bf6
COBWEBSTECH\HadasS <hadas.sayar@gmail.com>
2023-09-20 21:44:06 +0300

    Only server side ready

---
+++ BattleShipGame/BattleShipGame/Services/GameManagerService.cs
@@ -0,0 +1,78 @@
+﻿using BattleShipGame.Models;
+
+namespace BattleShipGame.Services
+{
+    public class GameManagerService
+    {
+        public List<Player> Players = new();
+        public Game game;
+        
+
+        public GameManagerService() 
+        {
+        
+        }
+
+        public Guid NewPlayer()
+        {
+            Player player;
+            if (Players.Count >= 2)
+            {
+                return Guid.Empty;
+            }
+            else
+            {
+                player = new Player();
+                Players.Add(player);
+                if (Players.Count == 2)
+                {
+                    game = new Game(Players);
+                }
+
+                return player.Id;    
+            }
+
+        }
+
+        public Board GetMyBoard(Guid playerId)
+        {
+            return (GetPlayerById(playerId).MyBoard);
+        }
+
+        public Board GetOppBoard(Guid playerId)
+        {
+            return (GetPlayerById(playerId).OppBoard);
+        }
+
+        private Player GetPlayerById(Guid playerId)
+        {
+            return Players.FirstOrDefault(p => p.Id.Equals(playerId));
+        }
+
+        public bool IsGameOver()
+        {
+            return game.IsGameOver();
+        }
+
+        public bool AmIWinner(Guid playerId)
+        {
+            return (game.GetWinner().Id == playerId);
+        }
+        
+        public string MakeMove((int, int) Cell, Guid playerId)
+
+        {
+
+                return game.MakeMove(Cell, playerId);
+
+            
+        }
+
+        public Guid WhosTurn()
+        {
+            return game.WhosTurn();
+        }
+
+        
+    }
+}
